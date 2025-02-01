c65bc0b942afe618c36fa4fa443645016c89045e
akhmadaliyev0918 <akhmadaliyev082@gmail.com>
2025-02-01 21:13:40 +0500

    first try

--- data/db.js
+++ data/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 // Connect to MongoDB
 const Database = () => {
-    mongoose.connect('mongodb+srv://akhmadaliyev082:N3T4HUN3YJ@cluster0.d7j3o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    mongoose.connect(process.env.MONGODB_API)
         .then(() => console.log('Connected to MongoDB'))
         .catch(err => {
             console.error("error connect to mongoDB", err);
--- index.js
+++ index.js
@@ -19,9 +19,7 @@ app.use(cors({
 
 
 app.get('/', (req, res) => {
-    res.json({
-        message: 'Welcome to the User API!',
-    });
+    res.send("Hello World!")
 });
 
 
@@ -41,5 +39,5 @@ app.use('/api/story', story);
 
 
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
